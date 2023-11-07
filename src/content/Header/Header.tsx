a6d5dfd3c9476b734b93ef63fc733260ccb45f7a
Kevin <kjrodgers26@gmail.com>
2023-11-07 10:08:56 -0500

    Added responsive design to header

--- src/content/Header/Header.tsx
+++ src/content/Header/Header.tsx
@@ -7,17 +7,18 @@ import InsertCommentIcon from "@mui/icons-material/InsertComment"
 import NotificationsIcon from "@mui/icons-material/Notifications"
 import PersonIcon from "@mui/icons-material/Person"
 import AppsIcon from "@mui/icons-material/Apps"
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
 
 const Header: React.FC = () => {
   return (
-    <div className="w-full flex justify-between align-center py-[.5rem] px-[2rem] bg-[#fff]">
-      <div className="flex align-center text-center items-center gap-5 w-full">
+    <div className="w-full flex align-center py-[.5rem] bg-[#fff] justify-center">
+      <div className="flex align-center text-center items-center gap-2 p-1">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png"
           alt="logo"
           className="w-[2rem] h-[2rem]"
         />
-        <div className="flex align-center bg-[#edf3f8] border-none outline-none items-center w-full">
+        <div className="flex align-center bg-[#edf3f8] border-none outline-none items-center w-10/12">
           <SearchIcon fontSize="small" />
           <input
             type="text"
@@ -27,34 +28,71 @@ const Header: React.FC = () => {
         </div>
       </div>
       <div className="flex align-center justify-evenly items-center">
-        <div className="flex-col text-center w-fit px-5">
-          <HomeIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs">Home</h2>
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer">
+          <HomeIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden">
+            Home
+          </h2>
         </div>
-        <div className="flex-col text-center w-fit px-5 whitespace-nowrap">
-          <PeopleAltIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs">My Network</h2>
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer whitespace-nowrap">
+          <PeopleAltIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden">
+            My Network
+          </h2>
         </div>
-        <div className="flex-col text-center w-fit px-5">
-          <BusinessCenterIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs">Jobs</h2>
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer">
+          <BusinessCenterIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden">
+            Jobs
+          </h2>
         </div>
-        <div className="flex-col text-center w-fit px-5">
-          <InsertCommentIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs">Messaging</h2>
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer">
+          <InsertCommentIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden">
+            Messaging
+          </h2>
         </div>
-        <div className="flex-col text-center w-fit px-5">
-          <NotificationsIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs">Notifications</h2>
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer">
+          <NotificationsIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden">
+            Notifications
+          </h2>
         </div>
-        <div className="flex-col text-center w-fit px-5">
-          <PersonIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs">Me</h2>
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer">
+          <PersonIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden">
+            Me
+          </h2>
         </div>
         <div className="border-l-[1px] text-color[#000000] h-full"></div>
-        <div className="flex-col text-center w-fit px-5">
-          <AppsIcon sx={{ color: "#666666", fontSize: 30 }} />
-          <h2 className="text-[#666666] font-light text-xs whitespace-nowrap">
+        <div className="flex-col text-center w-fit px-5 max-[660px]:px-2 cursor-pointer">
+          <AppsIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hover:text-black max-[520px]:hidden"
+          />
+          <MoreHorizIcon
+            sx={{ color: "#666666", fontSize: 30 }}
+            className="hidden max-[520px]:block"
+          />
+          <h2 className="text-[#666666] font-light text-xs max-md:hidden whitespace-nowrap">
             For Business
           </h2>
         </div>
